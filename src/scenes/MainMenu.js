cf8112c2b0135912ab53d062d29780deba74262a
superbrucem <superbrucem@gmail.com>
2025-04-16 15:46:52 -0400

    add music files

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -12,7 +12,21 @@ export default class MainMenu extends Phaser.Scene {
         super({ key: 'MainMenu' });
     }
 
+    preload() {
+        // Load the music file - supports mp3, ogg, wav
+        this.load.audio('bgMusic', [
+            'assets/audio/gamestart.mp3',
+            'assets/audio/gamestart.ogg'
+        ]);
+    }
+
     create() {
+        // Play the background music
+        this.sound.play('bgMusic', {
+            loop: true,
+            volume: 0.5
+        });
+
         const title = this.add.text(400, 200, 'My Game', {
             fontSize: FONT_SIZES.TITLE,
             fill: TEXT_COLOR
